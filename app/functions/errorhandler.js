d28e4140d09ba13cd85659bf28b98bb9312801e6
Simon Lepel <simbo@simbo.de>
2016-03-19 18:05:26 +0100

    move errorhandler to functions

--- app/functions/errorhandler.js
+++ app/functions/errorhandler.js
@@ -1,20 +1,21 @@
 'use strict';
 
-var logger = require('./logger');
+var logger = require('../modules/logger');
 
-function logError(err) {
+function errorhandler(err, req, res, next) {
+
+  err.status = err.status || 500;
+
+  err.url = req.url;
+
   if (err.status === 404) {
     logger.log('error', '%s %s %s %s', err.message, err.status, err.url);
   } else {
     logger.log('error', err.message, {status: err.status, url: err.url, stack: err.stack});
   }
-}
 
-function errorhandler(err, req, res, next) {
-  err.status = err.status || 500;
-  err.url = req.url;
-  logError(err);
   res.status(err.status);
+
   if (req.accepts('html')) {
     res.render('error', {
       page: {
@@ -29,6 +30,7 @@ function errorhandler(err, req, res, next) {
   } else {
     res.type('txt').send('ERROR: ' + err.message);
   }
+
 }
 
 module.exports = errorhandler;
