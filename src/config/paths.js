96c4f12682c6f2aa9b9021319cf9c5f4f90a9492
Simon Lepel <simbo@simbo.de>
2016-03-26 10:15:30 +0100

    add some comments to paths config

--- src/config/paths.js
+++ src/config/paths.js
@@ -2,18 +2,21 @@
 
 var path = require('path');
 
-var uberspace = require('./uberspace');
-
 var paths = {};
 
+// base folders
 paths.cwd = path.dirname(path.dirname(__dirname));
 paths.src = path.join(paths.cwd, 'src');
 paths.dest = path.join(paths.cwd, 'dest');
 
-paths.app = path.join(paths.src, 'app');
+// data folders, mapped to objects
 paths.config = path.join(paths.src, 'config');
 paths.data = path.join(paths.src, 'data');
 
+// node app
+paths.app = path.join(paths.src, 'app');
+
+// site contains everything to generate static content, assets and app views
 paths.site = {
   src: path.join(paths.src, 'site'),
   dest: path.join(paths.dest, 'www')
@@ -23,24 +26,29 @@ paths.layouts = path.join(paths.site.src, 'layouts');
 paths.static = path.join(paths.site.src, 'static');
 paths.views = path.join(paths.site.src, 'views');
 
+// assets root path
 paths.assets = {
   src: path.join(paths.src, 'assets'),
   dest: path.join(paths.site.dest, 'assets')
 };
+// javascript
 paths.js = {
   src: path.join(paths.assets.src, 'js'),
   dest: path.join(paths.assets.dest, 'js')
 };
+// stylesheets
 paths.css = {
   src: path.join(paths.assets.src, 'stylus'),
   dest: path.join(paths.assets.dest, 'css')
 };
 
+// nginx config paths to generate production version
 paths.nginx = {
   src: path.join(paths.cwd, '.provision/files/etc/nginx'),
   dest: path.join(paths.dest, 'nginx')
 };
 
-paths.remote = uberspace.paths;
+// remote production paths
+paths.remote = require('./uberspace').paths;
 
 module.exports = paths;
