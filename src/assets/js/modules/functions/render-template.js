e400603d54bce1d235be5aa1aa761c9665edd328
Simon Lepel <simbo@simbo.de>
2016-03-26 11:44:06 +0100

    fix indentation in module render-template

--- src/assets/js/modules/functions/render-template.js
+++ src/assets/js/modules/functions/render-template.js
@@ -16,23 +16,23 @@ var cache = {};
  * @return {HTMLCollection} rendered template
  */
 function renderTemplate(str, data) {
-    var dom = document.implementation.createHTMLDocument(),
-        fn = cache[str] = cache[str] ||
-            new Function('obj', // eslint-disable-line no-new-func
-                'var p=[],print=function(){p.push.apply(p,arguments);};' +
-                'with(obj){p.push(\'' +
-                str.replace(/[\r\t\n]/g, ' ')
-                    .split('{%').join('\t')
-                    .replace(/((^|%\})[^\t]*)"/g, '$1\r')
-                    .replace(/\t=(.*?)%\}/g, '\',$1,\'')
-                    .split('\t').join('\');')
-                    .split('%}').join('p.push(\'')
-                    .split('\r').join('"') +
-                '\');}return p.join("");');
-    data = typeof data === 'object' ? data : {};
-    dom.body.innerHTML = fn(data);
-    return dom.body.children.length > 1 ?
-        dom.body.children : dom.body.children[0];
+  var dom = document.implementation.createHTMLDocument(),
+      fn = cache[str] = cache[str] ||
+        new Function('obj', // eslint-disable-line no-new-func
+          'var p=[],print=function(){p.push.apply(p,arguments);};' +
+          'with(obj){p.push(\'' +
+          str.replace(/[\r\t\n]/g, ' ')
+            .split('{%').join('\t')
+            .replace(/((^|%\})[^\t]*)"/g, '$1\r')
+            .replace(/\t=(.*?)%\}/g, '\',$1,\'')
+            .split('\t').join('\');')
+            .split('%}').join('p.push(\'')
+            .split('\r').join('"') +
+          '\');}return p.join("");');
+  data = typeof data === 'object' ? data : {};
+  dom.body.innerHTML = fn(data);
+  return dom.body.children.length > 1 ?
+    dom.body.children : dom.body.children[0];
 }
 
 module.exports = renderTemplate;
