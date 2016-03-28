5a7e319435fd11647f2a488ce4c67b40741d7603
Simon Lepel <simbo@simbo.de>
2016-03-29 00:52:59 +0200

    add init submodule 'preparations'

--- src/app/modules/init/preparations.js
+++ src/app/modules/init/preparations.js
@@ -2,14 +2,25 @@
 
 var http = require('http');
 
-var io = require('socket.io');
+var cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    flash = require('connect-flash'),
+    io = require('socket.io');
 
-module.exports = populate;
+module.exports = prepare;
 
-function populate(app) {
+function prepare(app) {
 
   app.server = http.Server(app);
   app.io = io(app.server);
+
+  app.use(cookieParser());
+
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+  app.use(flash());
 
   return app;
 
