8b16b4aa5e62f11c1d4c314e408779c65507d635
Simon Lepel <simbo@simbo.de>
2016-03-28 18:42:10 +0200

    simplify ensureLoggedIn function of passport module

--- src/app/modules/passport.js
+++ src/app/modules/passport.js
@@ -31,15 +31,10 @@ module.exports = passport;
 module.exports.ensureLoggedIn = ensureLoggedIn;
 
 function ensureLoggedIn(options) {
-  var url, setReturnTo;
-  if (typeof options == 'string') options = {redirectTo: options};
-  options = options || {};
-  url = options.redirectTo || '/login';
-  setReturnTo = options.setReturnTo === undefined ? true : options.setReturnTo;
   return function(req, res, next) {
     if (!req.isAuthenticated || !req.isAuthenticated()) {
-      if (setReturnTo && req.session) req.session.returnTo = req.originalUrl || req.url;
-      return res.redirect(url);
+      if (req.session) req.session.returnTo = req.originalUrl || req.url;
+      return res.redirect('/login');
     }
     next();
   };
