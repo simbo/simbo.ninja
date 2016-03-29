15a06a3123fd6d7c2e3fb984d384743c2171b4fe
Simon Lepel <simbo@simbo.de>
2016-03-29 23:13:34 +0200

    refactor init module; combine smaller submodules

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,65 @@
 'use strict';
 
-var express = require('express');
+var http = require('http');
 
-var init = require('app/modules/init');
+var bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    express = require('express'),
+    flash = require('connect-flash'),
+    io = require('socket.io'),
+    Q = require('q');
 
-init(express());
+var config = require('config'),
+    auth = require('app/modules/auth'),
+    logger = require('app/modules/logger');
+
+// pipe app through setup steps
+Q(express())
+
+  // setup database layouts
+  .then(require('app/modules/init/databases'))
+
+  // setup common
+  .then(function(app) {
+    app.server = http.Server(app);
+    app.io = io(app.server);
+    app.use(cookieParser());
+    app.use(bodyParser.urlencoded({
+      extended: true
+    }));
+    app.use(flash());
+    return app;
+  })
+
+  // setup sessions
+  .then(require('app/modules/init/sessions'))
+
+  // setup authentication
+  .then(function(app) {
+    app.use(auth.initialize());
+    app.use(auth.session());
+    app.use(auth.addUserToLocals());
+    return app;
+  })
+
+  // setup views
+  .then(require('app/modules/init/views'))
+
+  // setup routes
+  .then(require('app/modules/init/routes'))
+
+  // setup errorhandling
+  .then(require('app/modules/init/errorhandling'))
+
+  // start listening
+  .then(function(app) {
+    app.server.listen(
+      config.app.server.port,
+      config.app.server.host,
+      function() {
+        var address = app.server.address();
+        logger.log('info', 'server started (listening on %s:%s)', address.address, address.port);
+      }
+    );
+    return app;
+  });
