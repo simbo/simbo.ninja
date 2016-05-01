b63e3abdd8a1efe022c7ddfb5d169fb8d8bf48fa
Simon Lepel <simbo@simbo.de>
2016-05-01 14:52:47 +0200

    fix nginx.conf for updated nginx version

--- .gulp/tasks/build/nginx-conf.js
+++ .gulp/tasks/build/nginx-conf.js
@@ -12,7 +12,7 @@ module.exports = [
       .pipe(this.plugins.replace('include /etc/nginx/sites-available/vagrant-couchdb;', ''))
       .pipe(this.plugins.replace('/etc/nginx', this.paths.remote.nginx))
       .pipe(this.plugins.replace('/vagrant/dest/www', path.join(this.paths.remote.root, 'dest/www')))
-      .pipe(this.plugins.replace('daemon on', 'daemon off'))
+      .pipe(this.plugins.replace('# daemon off;', 'daemon off;'))
       .pipe(this.gulp.dest(this.paths.nginx.dest));
   }
 
