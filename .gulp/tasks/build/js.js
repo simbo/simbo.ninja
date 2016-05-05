4b21df57568013b1c468aa836564c1f113c02385
Simon Lepel <simbo@simbo.de>
2016-05-05 11:50:34 +0200

    update eslint config and build:ja task to support angular2 apps

--- .gulp/tasks/build/js.js
+++ .gulp/tasks/build/js.js
@@ -23,16 +23,31 @@ function buildJs(done) {
         path.join(this.paths.js.src, 'modules'),
         path.join(this.paths.cwd, 'node_modules')
       ],
-      debug: this.env !== 'development',
-      transform: (this.env !== 'production' ? [eslintify] : [])
-        .concat([[babelify, {presets: 'es2015'}]])
-        .concat(this.env !== 'development' ? [[uglifyify, {global: true}]] : [])
+      debug: this.env === 'development',
+      transform: (this.env !== 'development' ? [eslintify] : [])
+        .concat([[babelify, {
+          presets: 'es2015',
+          plugins: [
+            'angular2-annotations',
+            'transform-decorators-legacy',
+            'transform-class-properties',
+            'transform-flow-strip-types'
+          ]
+        }]])
+        // .concat(this.env !== 'development' ? [[uglifyify, {
+        //   global: true
+        //   output: {
+        //     comments: /^=(require|include)\s.+/
+        //   }
+        // }]] : [])
     }
   };
 
   this.plugins.watchifyBrowserify('*.js', options, (stream) => stream
     .pipe(this.plugins.plumber())
     .pipe(this.plugins.sourcemaps.init({loadMaps: true}))
+    .pipe(this.plugins.include())
+    .pipe(this.env !== 'development' ? this.plugins.uglify() : this.util.noop())
     .pipe(this.plugins.sourcemaps.write('.', {includeContent: true, sourceRoot: '.'}))
     .pipe(this.gulp.dest(this.paths.js.dest))
     .on('end', this.reload)
---
+++ src/assets/js/app-libs.js
@@ -0,0 +1,3 @@
+//=require /node_modules/reflect-metadata/Reflect.js
+require('es6-shim');
+require('zone.js');
