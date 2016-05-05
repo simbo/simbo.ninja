9c549c48bdcb9fca1dc6c0b212231516cc4a3f01
Simon Lepel <simbo@simbo.de>
2016-05-05 11:57:37 +0200

    add dummy angular2 app 'picr'

---
+++ src/assets/js/modules/app/components/picr-app.js
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+const appName = 'picr';
+
+@Component({
+  selector: 'picr-app',
+  template: `<h1>${appName}</h1>`
+})
+
+export class PicrAppComponent {
+
+}
